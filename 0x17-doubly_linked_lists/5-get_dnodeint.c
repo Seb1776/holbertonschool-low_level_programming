61ac9ec2960275027f93e1fc932b4e8fbca3b994
Seb1776 <sebastianwho1776@gmail.com>
2021-05-10 04:29:03 +0000

    Added point 5, 5-get_dnodeint

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Selects the X item at given position
+ *
+ * @head: Start of list
+ * @index: Position of the item to print
+ *
+ * Return: The X element of the list
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    unsigned int i = 0;
+    dlistint_t *n;
+
+    if (head == NULL)
+        return (NULL);
+    
+    for (i = 0; i < index; i++)
+    {
+        if (head == NULL)
+            return (NULL);
+
+        n = head;
+        head = head->next;
+    }
+
+    return (n);
+}
