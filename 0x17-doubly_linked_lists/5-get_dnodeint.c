1e571a1a45229ef46f5199a02ae3d8c2198555b9
Seb1776 <sebastianwho1776@gmail.com>
2021-05-10 04:30:33 +0000

    Fixed conditional in for loop

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	for (i = 0; i < index; i++)
+	for (i = 0; i <= index; i++)
 	{
 		if (head == NULL)
 			return (NULL);
