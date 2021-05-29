0001415a12147eba18a9e594efb88a67f1acbf21
Seb1776 <sebastianwho1776@gmail.com>
2021-05-29 02:40:36 +0000

    Added point 0, 0-hash_table_create

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Function to create a hash table
+ *
+ * @size: Size of the table
+ *
+ * Return: New node
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_node;
+
+	new_node = malloc(sizeof(hash_table_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->array = calloc(size, sizeof(hash_table_t *));
+
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->size = size;
+	return (new_node);
+}
